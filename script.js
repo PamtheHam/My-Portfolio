24899ce1f6738310602908fc7f0b72d22c52be68
Pamela Hammond <hammondpamelac@gmail.com>
2021-08-01 21:34:14 -0400

    added new profile photo, screenshots of projects, adjusted tiles

---
+++ script.js
@@ -0,0 +1,34 @@
+// 
+// 1. [Updated Portfolio](#updated-portfolio)
+    // a. contact information
+    // b. links to github and linkedin
+    // c. downloadable pdf resume
+    // d. display 2 examples of student work with:
+        // project title, link to deployed version, link to repo, screenshot of app
+
+// 2. [Updated GitHub Profile](#updated-github-profile)
+    // a. a photo ❤️
+    // b. a bio ❤️
+    // c. email address ❤️
+    // d. link to deployed portfolio ❤️
+    // e. at least 2 pinned repos - every pinned repo should have a readme explaining the project, and contact info in the readme for collaborators
+    // f. follow other classmates on github ❤️
+
+// 3. [Updated Resume](#updated-resume)
+    // a. heading should include: name ❤️, phone number❤️, email address❤️, linkedin❤️, portfolio❤️, github❤️
+    // b. technical skills section with programming languages and technologies 
+    // c. 3 strongest projects/homework assignments with brief description, languages used, and url
+
+
+// 4. [Updated LinkedIn](#updated-linkedin) ❤️
+    // a. strong bio statement ❤️
+    // b. professional photo ❤️
+    // c. links to github and portfolio ❤️
+
+// 
+
+
+
+
+$(".dropdown-trigger").dropdown();
+$( document ).ready(function)
