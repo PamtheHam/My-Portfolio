d9398fde262b742292d0d8e24bf78186367638ec
Pamela Hammond <hammondpamelac@gmail.com>
2021-10-03 16:36:29 -0400

    not finished but added more styling to portfolio and new pictures

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,28 +1,12 @@
-// 
-// 1. [Updated Portfolio](#updated-portfolio)
-    // a. contact information
-    // b. links to github and linkedin
-    // c. downloadable pdf resume
-    // d. display 2 examples of student work with:
-        // project title, link to deployed version, link to repo, screenshot of app
+// document.addEventListener("DOMContentLoaded", function () {
+//   var elems = document.querySelectorAll(".sidenav");
+//   var instances = M.Sidenav.init(elems, options);
+// });
 
-// 2. [Updated GitHub Profile](#updated-github-profile)
-    // a. a photo ❤️
-    // b. a bio ❤️
-    // c. email address ❤️
-    // d. link to deployed portfolio ❤️
-    // e. at least 2 pinned repos - every pinned repo should have a readme explaining the project, and contact info in the readme for collaborators
-    // f. follow other classmates on github ❤️
-
-// 3. [Updated Resume](#updated-resume)
-    // a. heading should include: name ❤️, phone number❤️, email address❤️, linkedin❤️, portfolio❤️, github❤️
-    // b. technical skills section with programming languages and technologies 
-    // c. 3 strongest projects/homework assignments with brief description, languages used, and url
-
-
-// 4. [Updated LinkedIn](#updated-linkedin) ❤️
-    // a. strong bio statement ❤️
-    // b. professional photo ❤️
-    // c. links to github and portfolio ❤️
-
-// 
+$(document).ready(function () {
+  $(".sidenav").sideNav({
+    menuWidth: 300, // Default is 240
+    edge: "left",
+    draggable: true,
+  });
+});
